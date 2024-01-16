673745bd09eddd058d0582b51bb0c9d3c22680c1
Kinder <nguyenductrong12a1@gmail.com>
2024-01-16 23:46:15 +0700

    Add listener

--- src/main/java/com/kafkalab/quickstart/services/SongService.java
+++ src/main/java/com/kafkalab/quickstart/services/SongService.java
@@ -3,9 +3,12 @@ package com.kafkalab.quickstart.services;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import com.kafkalab.quickstart.dto.SongDTO;
 
 @Service
 public class SongService {
@@ -23,5 +26,10 @@ public class SongService {
                         message + "] due to : " + ex.getMessage());
             }
         });
+    }
+
+    @KafkaListener(id = "songGroup", topics = "song")
+    public void listen(SongDTO song) {
+        System.out.println("Received: " + song + "|" + song.getClass());
     }
 }
