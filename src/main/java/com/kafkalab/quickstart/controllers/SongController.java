fc3d7bf5c7edc98cba7c9fb1c084b551d19cc962
Kinder <nguyenductrong12a1@gmail.com>
2024-01-16 17:10:38 +0700

    Kafka integration

--- src/main/java/com/kafkalab/quickstart/controllers/SongController.java
+++ src/main/java/com/kafkalab/quickstart/controllers/SongController.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kafkalab.quickstart.dto.SongDTO;
+import com.kafkalab.quickstart.services.SongService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/songs")
 public class SongController {
+
+    @Autowired
+    private SongService songService;
+
     @PostMapping
     public ResponseEntity<?> createSong(@RequestBody SongDTO song) {
+        songService.sendMessage("song", song.getName());
         return ResponseEntity.status(201).body(song);
     }
 }
